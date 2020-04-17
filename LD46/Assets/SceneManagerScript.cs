2b4a4efb8ac006765206705c9540ad048c207239
Brett Miller <38405413+LLimb@users.noreply.github.com>
2020-04-17 14:12:35 -0600

    Scenes have been set up

---
+++ LD46/Assets/SceneManagerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("I have quit the game");
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("I am starting the game");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+    }
+
+    public void EndGame()
+    {
+        Debug.Log("I am entering the end of the game");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("End");
+    }
+
+    public void MainMenu()
+    {
+        Debug.Log("I am returning to the main menu");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+}
