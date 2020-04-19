f16b73da9b72990860c7ff288aee6cba9af07a28
Brett Miller <38405413+LLimb@users.noreply.github.com>
2020-04-19 17:08:33 -0600

    Fixed bug with inspecting plants

---
+++ LD46/Assets/ToggleInspectElementChildren.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleInspectElementChildren : MonoBehaviour
+{
+    public Image WaterLevelSprite;
+    public Image BackgroundSprite;
+    public Image RootsSprite;
+    public Image ContainerSprite;
+
+    public void ToggleElementsOn(Vector3 waterPos)
+    {
+        WaterLevelSprite.GetComponent<RectTransform>().localPosition = waterPos;
+        ToggleSprites(true);
+    }
+
+    public void ToggleELementsOff()
+    {
+        ToggleSprites(false);
+    }
+
+    private void ToggleSprites(bool val)
+    {
+        WaterLevelSprite.enabled = val;
+        BackgroundSprite.enabled = val;
+        RootsSprite.enabled = val;
+        ContainerSprite.enabled = val;
+    }
+}
--- LD46/Assets/UI-Tools/InspectPlanterTool.cs
+++ LD46/Assets/UI-Tools/InspectPlanterTool.cs
@@ -7,38 +7,43 @@ using UnityEngine.UI;
 public class InspectPlanterTool : MonoBehaviour
 {
     PlantControllerScript PC;
-    public Image WaterLevelSprite;
-    public Image BackgroundSprite;
-    public Image RootsSprite;
-    public Image ContainerSprite;
+    ToggleInspectElementChildren ToggleSprites;
+    //public Image WaterLevelSprite;
+    //public Image BackgroundSprite;
+    //public Image RootsSprite;
+    //public Image ContainerSprite;
 
     private void Start()
     {
         this.PC = GetComponent<PlantControllerScript>();
+        this.ToggleSprites = FindObjectOfType<ToggleInspectElementChildren>();
     }
 
     private void OnMouseOver()
     {
         if (ToolManagerScript.Instance.CheckActiveTool(ToolManagerScript.Tools.Inspect))
         {
-            if(PC._Plant != null)
+            if(PC._Plant != null && ToggleSprites != null)
             {
                 int waterFactor = PC.GetRootWaterFactor();
 
                 if (waterFactor == 1)
                 {
-                    WaterLevelSprite.GetComponent<RectTransform>().localPosition = new Vector3(0, 245, 0);
+                    //WaterLevelSprite.GetComponent<RectTransform>().localPosition = new Vector3(0, 245, 0);
+                    ToggleSprites.ToggleElementsOn(new Vector3(0, 245, 0));
                 }
                 else if (waterFactor == 0)
                 {
-                    WaterLevelSprite.GetComponent<RectTransform>().localPosition = new Vector3(0, 205, 0);
+                    //WaterLevelSprite.GetComponent<RectTransform>().localPosition = new Vector3(0, 205, 0);
+                    ToggleSprites.ToggleElementsOn(new Vector3(0, 205, 0));
                 }
                 else if (waterFactor == -1)
                 {
-                    WaterLevelSprite.GetComponent<RectTransform>().localPosition = new Vector3(0, 155, 0);
+                    //WaterLevelSprite.GetComponent<RectTransform>().localPosition = new Vector3(0, 155, 0);
+                    ToggleSprites.ToggleElementsOn(new Vector3(0, 155, 0));
                 }
 
-                ToggleSprites(true);
+                //ToggleSprites(true);
             }
         }
     }
@@ -47,15 +52,8 @@ public class InspectPlanterTool : MonoBehaviour
     {
         if (ToolManagerScript.Instance.CheckActiveTool(ToolManagerScript.Tools.Inspect))
         {
-            ToggleSprites(false);
+            //ToggleSprites(false);
+            ToggleSprites.ToggleELementsOff();
         }
-    }
-
-    private void ToggleSprites(bool val)
-    {
-        WaterLevelSprite.enabled = val;
-        BackgroundSprite.enabled = val;
-        RootsSprite.enabled = val;
-        ContainerSprite.enabled = val;
     }
 }
